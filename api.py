73d0c72f7081c40f377b412e72420809d044d019
firstgenius <73236714+firstgenius@users.noreply.github.com>
2021-05-15 15:51:32 +0300

    Add files via upload

--- api.py
+++ api.py
@@ -1,75 +1,116 @@
 '''
-Google Cloud Vision API example.
+Google Cloud Vision API processing.
 '''
 
-import io
 import os
 from google.cloud import vision
 
 class Knot:
-    """Lightweight, nonpublic
-    class for storing a singly linked node.
     """
-
+    Lightweight, nonpublic
+class for storing a singly linked node.
+    """
     __slots__ = "_element", "_next"     
 
     def __init__(self, element = None, next = None):  
+        """
+        Inititialization of an instance of the object.
+
+        Args:
+            element ([object], optional): [value to store in the 'Knot']. Defaults to None.
+            next ([object], optional): [object linked next to the one]. Defaults to None.
+        """        
         self._element = element         
         self._next = next     
 
     @property
     def element(self):
+        """
+        element property for '_element' storage.
+
+        Returns:
+            [object]: [the element stored in the _element property].
+        """        
         return self._element
     
     @element.setter
     def element(self, value):
+        """
+        Setter function for element property.
+
+        Args:
+            value ([object]): [object to set as a value of the _element property].
+        """        
         self._element = value
     
     @property
     def next(self):
+        """
+        next property for '_next' storage. 
+
+        Returns:
+            [object]: [the element stored in the _next property].
+        """        
         return self._next
     
     @next.setter
     def next(self, value):
+        """
+        Setter function for element property.
+
+        Args:
+            value ([object]): [object to set as a value of the _next property].
+        """        
         self._next = value
 
 class Cream:
-    def __init__(self):
-        '''
-        The initialization of an instance of the object.
-        '''
-        self.ingrediets = Knot() 
-        self.damage_rate = self.callculate_damate_rate()
+    def check_ingredients(self, photo, dataset):
+            """
+            Check the ingredients.
 
-    def check_ingredients(self, path, data):
-        '''
-        Forms a list of ingredients in the 'cream'.
-        '''
-        ingredients_words = self.detect_ingredients(path)
-        ingredients = self.ingrediets
-        for ingredient in ingredients_words:
-            entry = (Ingredient())
-            if data.is_find(ingredient):
-                entry = Ingredient(data.retrieve(ingredient))
-            ingredients.next = Knot()
-            ingredients = ingredients.next
+            Args:
+                photo ([type]): [description]
+                dataset ([type]): [description]
+
+            Returns:
+                [type]: [description]
+            """
+            ingredients_words = self.find_ingredients(photo)
+            ingredients = Knot()
+            ingredients_list_head = ingredients
+            for ingredient in ingredients_words:
+                ingredients.element = dataset.retrieve(ingredient)
+                ingredients.next = Knot()
+                ingredients = ingredients.next
+
+            return ingredients_list_head
 
     def find_ingredients(self, path):
-        '''
-        Finds the ingredients of the file on the given path.
-        '''
+        """
+        Finds ingredients on a photo.
+
+        Args:
+            path ([str]): [path to a photo].
+
+        Returns:
+            [list]: [list of words found on the photo].
+        """
         detected_text = self.detect_text(path)
         detected_text_words = self.process_text(detected_text)
         return detected_text_words
 
-    def callculate_damate_rate(self):
-        pass
-
     @staticmethod
     def detect_text(photo):
-        """Detects text in the file."""
+        """
+        Detects and returns text on a photo.
 
-        os.environ['GOOGLE_APPLICATION_CREDENTIALS']= r'/Users/ya_tvoy_bro/Documents/python_projects/testtask_MacPaw/MacPaw-testtask/CosmeticsBot/simpletextrecognition.json'
+        Args:
+            photo ([str]): [photo in 64bit encoded string].
+
+        Raises:
+            Exception: [Exception]
+        """
+        os.environ['GOOGLE_APPLICATION_CREDENTIALS']= r'simpletextrecognition.json'
         client = vision.ImageAnnotatorClient()
 
         image = vision.Image(content=photo)
@@ -83,23 +124,35 @@ class Cream:
                 '{}\nOoops! Some error occured! Sorry for the troubles.\
 Try making a different image, if nothing changes please try again later.'.format(
                     detected_text.error.message))
-        
+
         return detected_text
 
     @staticmethod
     def process_text(detected_text):
-        '''
-        Processes retrieved container with text.
-        '''
+        """
+        Processes texts found on a photo and returns words.
+
+        Args:
+            detected_text ([type]): [description]
+
+        Returns:
+            [list]: [list of words found on the photo].
+        """
         detected_text_strign = detected_text[0].description
 
-        detected_text_words = detected_text_strign.split()
+        detected_text_words = detected_text_strign.split(',')
 
         return detected_text_words
 
 class Ingredient:
-    __slots__ = ('name', 'damage_rate', 'properties')
-    def check(self, entry):
+    __slots__ = ('name', 'safety_assesment', 'description')
+    def fill_up(self, entry):
+        """
+        Fills up the properties of the instance.
+
+        Args:
+            entry ([dict]): [dictionary with words to fill up the properties of the instance].
+        """
         self.name = entry['name']
-        self.damage_rate = entry['Damage_rate']
-        self.properties = entry['Properties']
+        self.safety_assesment = entry['property0']
+        self.description = entry['property1']
--- tg_bot.py
+++ tg_bot.py
@@ -1,20 +1,24 @@
 import telebot
-import schedule
-from threading import Thread
 from telebot import types
-from datetime import date, datetime
-from time import sleep
 from api import Cream
 import os
+from dataset_ADT import DataframeDataset
+
 #Enter API_KEY from file 'Keys'
-API_KEY = ''
+API_KEY = '1886309037:AAE0jcZvgf1fuXJbokWmnyT40HFUElBzfzw'
 bot = telebot.TeleBot(API_KEY)
 
 markup_for_yes_no = types.InlineKeyboardMarkup()
 item_yes = types.InlineKeyboardButton(text='YES', callback_data='yes')
 item_no = types.InlineKeyboardButton(text='NO', callback_data='no')
 markup_for_yes_no.add(item_yes, item_no)
+ingredients = None
 
+cream_procesor = Cream()
+forbidden_dataset = DataframeDataset()
+description_dataset = DataframeDataset()
+forbidden_dataset.read_data('x2_new.xlsx')
+description_dataset.read_data('x_new.xlsx')
 
 @bot.message_handler(commands=['start'])
 def start(message):
@@ -26,30 +30,75 @@ def add_photo(message):
     bot.send_message(message.chat.id, 'Please send a photo of your cosmetic ingredients.')
     bot.register_next_step_handler(message, photo)
 
-
 @bot.callback_query_handler(func = lambda call: True)
 def answer(call):
     if call.data == 'yes':
         bot.delete_message(call.message.chat.id, call.message.message_id)
-        bot.send_message(call.message.chat.id, 'Here is your info')
+        global ingredients
+        head = ingredients.next
+        while head is not None:
+            ingredient = head.element
+            if ingredient is not None:
+                diplay_ingredient = decorate_ingredient_display(ingredient)
+                bot.send_message(call.message.chat.id, diplay_ingredient)
+
+            head = head.next
+
         bot.send_message(call.message.chat.id, 'Fine. Thanks for using our service! To upload new photo -> /sendphoto\nSEE YOU LATER❤️')
     elif call.data == 'no':
         bot.send_message(call.message.chat.id, 'Fine. Thanks for using our service! To upload new photo -> /sendphoto\nSEE YOU LATER❤️')
 
 
+def cut_spaces(string):
+    string = string.split()
+    string = ' '.join(string)
+    return string
+
+def decorate_ingredient_display(ingredient):
+    strign = ''
+
+    ingredient_name = ingredient['name']
+    ingredient_assesment = ingredient['property0']
+    ingredient_conclusion = cut_spaces(ingredient['property1'])
+
+    ingredient_name_prefix = f'Detected ingredient: {ingredient_name}'
+    safety_assesment_afix = f'Researches assesed with finding: {ingredient_assesment}'
+    scientific_conclusion_sufix = f'Researche\'s conclusion: {ingredient_conclusion}'
+
+    beaty_rafix = '^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^'
+    beaty_japix = '__________________________________________'
+    beaty_gaxix = '------------------------------------------'
+
+    text = f'{beaty_rafix}\n{ingredient_name_prefix}\n{beaty_gaxix}\n{safety_assesment_afix}\n{beaty_gaxix}\n{scientific_conclusion_sufix}\n{beaty_japix}' 
+
+    return text
+
 def photo(message):
         try:
             file_info = bot.get_file(message.photo[len(message.photo) - 1].file_id)
             downloaded_file = bot.download_file(file_info.file_path)
-            lyst = Cream.find_ingredients(Cream(), downloaded_file)
-            print(lyst)
             bot.reply_to(message, "Photo is being processed")
+            global ingredients 
+            ingredients = cream_procesor.check_ingredients(downloaded_file, description_dataset)
+
+            forbiden_ingredients = cream_procesor.check_ingredients(downloaded_file, forbidden_dataset)
+            head = forbiden_ingredients.next
+            text = ''
+            while head is not None:
+                forbiden_ingredient = head.element
+                if forbiden_ingredient:
+                    text += str(forbiden_ingredient['name']) + '\n'
+                head = head.next
+            if text == '':
+                bot.send_message(message.chat.id, 'There is no prohibited(dangerous) ingredients in this product')
+            else:
+                bot.send_message(message.chat.id, 'There are such prohibited(dangerous) ingredients in this product:\n' + text)
+                
             bot.send_message(message.chat.id, "Do you want to get more info about ingredients?",
                 reply_markup=markup_for_yes_no
             )
         except:
             bot.send_message(message.chat.id, "Invalid photo. To try again - send /sendphoto")
-
 
 if __name__ == "__main__":
     bot.polling()
